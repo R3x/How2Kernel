420b9241337b2aa99c849c1bfe4b997eedac98cd
R3x <siddharth.muralee@gmail.com>
2018-11-01 13:10:02 +0530

    Restructured setup.sh and readme

--- README.md
+++ README.md
@@ -6,12 +6,15 @@ This Repository aims at giving a basic idea about Kernel Exploitation.
 
 - Clone the repository to your system `git clone https://github.com/R3x/How2Kernel ~/How2Kernel`.
 - Go to the folder containing the setup `cd ~/How2kernel`
-- run setup.sh to setup the Lab and testing environment `./setup.sh`
+- run setup.sh to setup the Lab and testing environment
+ - In case you want to run the modules in your own system `./setup.sh -local`
+ - To build a virtual environment with QEMU `./setup.sh -virt -<architecture>`.
 - Start Hacking the kernel !!!
 
 * Make sure there are no spaces in the path to the directory that you have cloned. This will cause errors while building.
+* Architectures supported currently `-arm` for Aarch 64 architecture and `-x86` for amd64 architecture.
 
-## Labs
+ ## Labs
 
 ### Lab 1 - Intro to Kernel and Kernel Modules
 
--- setup.sh
+++ setup.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 
-# Install qemu for running the kernel Images
-
-sudo apt update
-
-
-if [ $1 == '-arm' ]
+if [ $# -eq 0 ]
 then
-    # GDB multiarch for Debugging
-    sudo apt-get install -y gdb-multiarch
-    # Cross Compiler for arm
-    sudo apt-get install -y gcc-arm-linux-gnueabihf libc6-dev-armhf-cross libelf-dev
-elif [ $1 == '-x86' ]
+    echo "Please supply the needed arguments\n"
+    exit
+fi
+
+if [ $1 == '-local' ]
 then
-    echo "Building for x86"
+    sudo apt update
+    echo -e "\n---- Installing Kernel Headers -----\n\n"
     sudo apt install -y linux-headers-$(uname -r)
 elif [ $1 == '-virt' ]
-then
+    then
+    # Install qemu for running the kernel Images
+    sudo apt update
+    echo -e "\n---- Installing QEMU -----\n\n"
     sudo apt-get install -y qemu qemu-user qemu-user-static
+    if [ $2 == 'arm'  ]
+        then
+        # GDB multiarch for Debugging
+        sudo apt-get install -y gdb-multiarch
+        # Cross Compiler for arm
+        sudo apt-get install -y gcc-arm-linux-gnueabihf libc6-dev-armhf-cross libelf-dev
+    elif [ $2 == '-x86' ]
+        then
+        echo "Building for x86"
+    else
+        echo "Unknown parameter $2"
+    fi
+    echo -e "\n---- Installing Kernel Sources -----\n\n"
+    mkdir kernel_source
+    cd kernel_source
+    wget -c  https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.18.16.tar.xz
+    tar -xvf linux-4.18.16.tar.xz
 else
     echo "Unknown parameter $1"
 fi
 
-# mkdir kernel_source
-# cd kernel_source
-# wget -c  https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.18.16.tar.xz
-# tar -xvf linux-4.18.16.tar.xz
-# cd 
 
 
